03f920ed8b5a5b098f960ee400b374377e2cfd97
Cockpit Cutie <cockpitcutie@gmail.com>
2025-06-08 02:30:08 -0500

    add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -6,26 +6,53 @@ pub use shape::{Constant, SineWave, SquareWave};
 pub trait Pattern: Sized {
     fn sample(&self, time: f64) -> f64;
 
+    /// Scales the pattern in the time domain by a given `scalar`.
+    /// 
+    /// For example, a scalar of 2.0 would double the length of cycles.
+    /// This would turn a sine wave of wavelength 0.5 seconds into a square wave of wavelength 1.0 seconds.
     fn scale_time(self, scalar: f64) -> ScaleTime<Self> {
         ScaleTime { pattern: self, scalar }
     }
 
+    /// Scales the pattern in the intensity domain by a given `scalar`.
+    /// 
+    /// For example, a scalar of 2.0 would double the intensity of the pattern.
+    /// This would turn a sine wave of amplitude 0.5 into a square wave of amplitude 1.0.
     fn scale_intensity(self, scalar: f64) -> ScaleIntensity<Self> {
         ScaleIntensity { pattern: self, scalar }
     }
 
+    /// Takes the sum of two patterns.
+    /// 
+    /// For example, a sine wave of amplitude 0.5 and a square wave of amplitude 0.5 would sum to a sine wave of amplitude 1.0.
     fn sum<Q: Pattern>(self, other: Q) -> Sum<Self, Q> {
         Sum { a: self, b: other }
     }
 
+    /// Takes the difference of two patterns.
+    /// 
+    /// For example, a sine wave of amplitude 0.75 and a square wave of amplitude 0.25 would subtract to a sine wave of amplitude 0.5.
+    fn subtract<Q: Pattern>(self, other: Q) -> Subtract<Self, Q> {
+        self.sum(other.scale_intensity(-1.0))
+    }
+
+    /// Takes the average of two patterns.
+    /// 
+    /// For example, a sine wave of amplitude 0.75 and a square wave of amplitude 0.25 would average to a sine wave of amplitude 0.5.
     fn average<Q: Pattern>(self, other: Q) -> Average<Self, Q> {
         Average { a: self, b: other }
     }
 
+    /// Clamps the pattern to a given range.
+    /// 
+    /// This is useful for limiting the output of a pattern to a certain range.
+    /// Or making waves that clip at the edges of the range.
     fn clamp(self, floor: f64, ceiling: f64) -> Clamp<Self> {
         Clamp { pattern: self, floor, ceiling }
     }
 
+    /// Clamps the pattern to a valid scalar value for a buttplug command.
+    /// This is in the range of 0.0 to 1.0.
     fn clamp_valid(self) -> Clamp<Self> {
         self.clamp(0.0, 1.0)
     }
@@ -38,7 +65,7 @@ pub struct ScaleTime<P: Pattern> {
 
 impl<P: Pattern> Pattern for ScaleTime<P> {
     fn sample(&self, time: f64) -> f64 {
-        self.pattern.sample(self.scalar * time)
+        self.pattern.sample(self.scalar * (1.0 / time))
     }
 }
 
@@ -63,6 +90,8 @@ impl<P: Pattern, Q: Pattern> Pattern for Sum<P, Q> {
         self.a.sample(time) + self.b.sample(time)
     }
 }
+
+type Subtract<P, Q> = Sum<P, ScaleIntensity<Q>>;
 
 pub struct Average<P: Pattern, Q: Pattern> {
     a: P,
