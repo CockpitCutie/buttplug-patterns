88b98c7021c7131740623ef95235c71e233b1e21
Cockpit Cutie <cockpitcutie@gmail.com>
2025-06-08 01:49:46 -0500

    implement scalars, sum, average, and clamps

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,88 @@
-use buttplug::client::{ButtplugClient, LinearCommand, RotateCommand, ScalarCommand};
-
 pub mod driver;
 mod shape;
 
-pub use shape::{Constant, SquareWave, SineWave};
-
-pub enum Command {
-    Linear(LinearCommand),
-    Rotate(RotateCommand),
-    Scalar(ScalarCommand),
-}
+pub use shape::{Constant, SineWave, SquareWave};
 
 pub trait Pattern: Sized {
     fn sample(&self, time: f64) -> f64;
 
-    fn scale_time(self, scalar: f64) -> ScaleTime {
-        todo!()
+    fn scale_time(self, scalar: f64) -> ScaleTime<Self> {
+        ScaleTime { pattern: self, scalar }
     }
 
-    fn scale_intensity(self, scalar: f64) -> ScaleIntensity {
-        todo!()
+    fn scale_intensity(self, scalar: f64) -> ScaleIntensity<Self> {
+        ScaleIntensity { pattern: self, scalar }
     }
 
-    fn sum(self, other: impl Pattern) -> Sum {
-        todo!()
+    fn sum<Q: Pattern>(self, other: Q) -> Sum<Self, Q> {
+        Sum { a: self, b: other }
+    }
+
+    fn average<Q: Pattern>(self, other: Q) -> Average<Self, Q> {
+        Average { a: self, b: other }
+    }
+
+    fn clamp(self, floor: f64, ceiling: f64) -> Clamp<Self> {
+        Clamp { pattern: self, floor, ceiling }
+    }
+
+    fn clamp_valid(self) -> Clamp<Self> {
+        self.clamp(0.0, 1.0)
     }
 }
 
-struct ScaleTime {}
+pub struct ScaleTime<P: Pattern> {
+    pattern: P,
+    scalar: f64,
+}
 
-struct ScaleIntensity{}
+impl<P: Pattern> Pattern for ScaleTime<P> {
+    fn sample(&self, time: f64) -> f64 {
+        self.pattern.sample(self.scalar * time)
+    }
+}
 
-struct Sum {}
+pub struct ScaleIntensity<P: Pattern> {
+    pattern: P,
+    scalar: f64,
+}
+
+impl<P: Pattern> Pattern for ScaleIntensity<P> {
+    fn sample(&self, time: f64) -> f64 {
+        self.scalar * self.pattern.sample(time)
+    }
+}
+
+pub struct Sum<P: Pattern, Q: Pattern> {
+    a: P,
+    b: Q,
+}
+
+impl<P: Pattern, Q: Pattern> Pattern for Sum<P, Q> {
+    fn sample(&self, time: f64) -> f64 {
+        self.a.sample(time) + self.b.sample(time)
+    }
+}
+
+pub struct Average<P: Pattern, Q: Pattern> {
+    a: P,
+    b: Q
+}
+
+impl<P: Pattern, Q: Pattern> Pattern for Average<P, Q> {
+    fn sample(&self, time: f64) -> f64 {
+        (self.a.sample(time) + self.b.sample(time)) / 2.0
+    }
+}
+
+pub struct Clamp<P: Pattern> {
+    pattern: P,
+    ceiling: f64,
+    floor: f64,
+}
+
+impl<P: Pattern> Pattern for Clamp<P> {
+    fn sample(&self, time: f64) -> f64 {
+        self.pattern.sample(time).max(self.floor).min(self.ceiling)
+    }
+}
