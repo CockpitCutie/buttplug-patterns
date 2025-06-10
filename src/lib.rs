c045db1072a2e638750488b60c28d63c971831fd
Cockpit Cutie <cockpitcutie@gmail.com>
2025-06-10 13:10:00 -0500

    add per-device and per-actuator patterns

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
-pub mod shapes;
 pub mod random;
+pub mod shapes;
 pub mod transformers;
 
 use std::{
-    sync::{atomic::{AtomicBool, Ordering}, Arc},
+    collections::HashMap,
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    },
     time::{Duration, Instant},
 };
 
@@ -94,7 +98,7 @@ pub trait Pattern: PatternGenerator + Sized {
     }
 
     /// Scales a pattern to a valid range for a buttplug command.
-    /// 
+    ///
     /// Scaling is performed by a sigmoid function 1/(1+e^(-x)).
     fn scale_valid(self) -> ValidScale<Self> {
         ValidScale { pattern: self }
@@ -154,6 +158,8 @@ pub struct Driver {
     pub buttplug: Arc<ButtplugClient>,
     tickrate_hz: u64,
     pattern: Box<dyn PatternGenerator>,
+    device_patterns: HashMap<u32, Box<dyn PatternGenerator>>,
+    actuator_patterns: HashMap<(u32, u32), Box<dyn PatternGenerator>>,
 }
 
 impl Driver {
@@ -166,15 +172,60 @@ impl Driver {
             buttplug: bp,
             tickrate_hz: 10, // 10 hz is fast enough to feel smooth without overwhelming the device or server in my testing
             pattern: Box::new(pattern),
+            device_patterns: HashMap::new(),
+            actuator_patterns: HashMap::new(),
         }
     }
 
-    /// Sets the tickrate of the driver, in Hz. The tickrate is the number of times per second 
+    /// Sets the tickrate of the driver, in Hz. The tickrate is the number of times per second
     /// that the driver samples the pattern and send the new intensity to the device.
-    /// 
+    ///
     /// The default tickrate is 10 Hz.
     pub fn set_tickrate(&mut self, hz: u64) -> &mut Self {
         self.tickrate_hz = hz;
+        self
+    }
+
+    /// Sets the global pattern of the driver.
+    /// This pattern is applied to all actuators on all devices that do not have a more specific pattern.
+    pub fn set_pattern<P: 'static + PatternGenerator>(&mut self, pattern: P) -> &mut Self {
+        self.pattern = Box::new(pattern);
+        self
+    }
+
+    /// Sets the pattern of a specific device based on its index.
+    ///
+    /// Device indexes can be found using the `index()` method of the `ButtplugClientDevice`.
+    pub fn set_device_pattern<P: 'static + PatternGenerator>(
+        &mut self,
+        device_id: u32,
+        pattern: P,
+    ) -> &mut Self {
+        self.device_patterns.insert(device_id, Box::new(pattern));
+        self
+    }
+
+    /// Removes the pattern of a specific device based on its ID.
+    pub fn remove_device_pattern(&mut self, device_id: u32) -> &mut Self {
+        self.device_patterns.remove(&device_id);
+        self
+    }
+
+    /// Sets the pattern of a specific actuator based on its device ID and actuator ID.
+    pub fn set_actuator_pattern<P: 'static + PatternGenerator>(
+        &mut self,
+        device_id: u32,
+        actuator_id: u32,
+        pattern: P,
+    ) -> &mut Self {
+        self.actuator_patterns
+            .insert((device_id, actuator_id), Box::new(pattern));
+        self
+    }
+
+    /// Removes the pattern of a specific device based on its ID.
+    pub fn remove_actuator_pattern(&mut self, device_id: u32, actuator_id: u32) -> &mut Self {
+        self.actuator_patterns.remove(&(device_id, actuator_id));
         self
     }
 
@@ -188,14 +239,28 @@ impl Driver {
                 break;
             }
 
+            let global_intensity = self.pattern.sample(elapsed);
             for device in self.buttplug.devices() {
-                let level = self.pattern.sample(elapsed);
+                let mut actuator_map: HashMap<u32, f64> = HashMap::new();
+                for actuator in device.vibrate_attributes() {
+                    // vibrate attributes returns a vec of actuator info
+                    let level = self
+                        .actuator_patterns
+                        .get_mut(&(device.index(), *actuator.index()))
+                        .map(|p| p.sample(elapsed))
+                        .unwrap_or(
+                            self.device_patterns
+                                .get_mut(&device.index())
+                                .map(|p| p.sample(elapsed))
+                                .unwrap_or(global_intensity),
+                        );
+                    actuator_map.insert(*actuator.index(), level);
+                }
                 device
-                    .vibrate(&ScalarValueCommand::ScalarValue(level))
+                    .vibrate(&ScalarValueCommand::ScalarValueMap(actuator_map))
                     .await
                     .unwrap();
             }
-
             interval.tick().await;
         }
         self.buttplug.stop_all_devices().await.unwrap();
