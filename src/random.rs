d1afa036fd1046c30288997b6b7bc4eaea8b51f8
Cockpit Cutie <cockpitcutie@gmail.com>
2025-06-09 22:58:11 -0500

    add random type

---
+++ src/random.rs
@@ -0,0 +1,25 @@
+use std::ops::Range;
+
+use crate::PatternGenerator;
+
+/// Generates a random value between the given range every tick.
+pub struct Random {
+    pub range: Range<f64>,
+    pub duration: f64,
+}
+
+impl Random {
+    pub fn new(range: Range<f64>, duration: f64) -> Self {
+        Random { range, duration }
+    }
+}
+
+impl PatternGenerator for Random {
+    fn sample(&self, _time: f64) -> f64 {
+        rand::random_range(self.range.clone())
+    }
+
+    fn duration(&self) -> f64 {
+        self.duration
+    }
+}
